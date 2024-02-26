8873817c2a04565ccefadf7a77590342f47cd2f3
WhiteJohny <vvvfrog2004@gmail.com>
2024-02-26 18:24:45 +0300

    Удалены за ненадобностью

--- bot/secrets.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-TG_API_KEY = os.getenv("TG_API_KEY")
--- bot/utills.py
+++
@@ -1,9 +0,0 @@
-from urllib.request import urlopen
-
-
-def url_validation(url: str) -> bool:
-    try:
-        urlopen(url)
-    except Exception:
-        return False
-    return True
