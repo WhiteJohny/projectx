a1fa9153628f830581b6ea7a873585b2d6b1475f
WhiteJohny <vvvfrog2004@gmail.com>
2024-03-27 01:04:36 +0300

    abc fix

--- src/parser/parsers/not_ready/abc.py
+++ src/parser/parsers/not_ready/abc.py
@@ -1,22 +1,22 @@
-import requests
-from bs4 import BeautifulSoup as bs
-
-url1 = "https://abcnews.go.com/International/wireStory/chinese-coast-guard-blocks-vessels-off-south-china-107795637"
-url = "https://abcnews.go.com/International/israel-hamas-talks-cease-fire-hostage-deal-gaza-war/story?id=107801137"
-
-r = requests.get(blogs)
-
-soup = bs(r.content, "html.parser")
-
-article = soup.find("div", class_="xvlfx ZRifP TKoO eaKKC bOdfO")
-
-news_title = soup.find("h1").text
-
-text = ''
-
-for p in article.findAll('p'):
-    if p.text:
-        text += p.text + '\n'
-
-print(news_title)
-print(text)
+# import requests
+# from bs4 import BeautifulSoup as bs
+#
+# url1 = "https://abcnews.go.com/International/wireStory/chinese-coast-guard-blocks-vessels-off-south-china-107795637"
+# url = "https://abcnews.go.com/International/israel-hamas-talks-cease-fire-hostage-deal-gaza-war/story?id=107801137"
+#
+# # r = requests.get(blogs)
+#
+# soup = bs(r.content, "html.parser")
+#
+# article = soup.find("div", class_="xvlfx ZRifP TKoO eaKKC bOdfO")
+#
+# news_title = soup.find("h1").text
+#
+# text = ''
+#
+# for p in article.findAll('p'):
+#     if p.text:
+#         text += p.text + '\n'
+#
+# print(news_title)
+# print(text)
