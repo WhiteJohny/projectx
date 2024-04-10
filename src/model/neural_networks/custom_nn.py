b7b0c988c41d0e4165eedfbcb9fde36bdb5aa5f9
LiquidIce64 <gilyazov.rm2005@gmail.com>
2024-04-10 14:53:17 +0300

    merge model to main

---
+++ src/model/neural_networks/custom_nn.py
@@ -0,0 +1,158 @@
+import json
+import time
+import clearml
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+
+
+class CustomNN:
+    @dataclass
+    class __Layer:
+        weights: any
+        biases: any
+
+    def __init__(self, input_size: int, output_size: int, hidden_size: int, hidden_count: int, random_seed: int = None):
+        if random_seed is not None:
+            np.random.seed(random_seed)
+        self.layers = []
+        if hidden_count > 0:
+            self.layers.append(self.__Layer(
+                weights=np.random.randn(hidden_size, input_size),
+                biases=np.zeros(hidden_size)
+            ))
+            for i in range(hidden_count - 1):
+                self.layers.append(self.__Layer(
+                    weights=np.random.randn(hidden_size, hidden_size),
+                    biases=np.zeros(hidden_size)
+                ))
+            self.layers.append(self.__Layer(
+                weights=np.random.randn(output_size, hidden_size),
+                biases=np.zeros(output_size)
+            ))
+        else:
+            self.layers.append(self.__Layer(
+                weights=np.random.randn(output_size, input_size),
+                biases=np.zeros(output_size)
+            ))
+
+    @staticmethod
+    def from_file(filepath: str):
+        with open(filepath, "r") as f:
+            layers = json.load(f)
+        network = CustomNN.__new__(CustomNN)
+        network.layers = [CustomNN.__Layer(
+            weights=np.array(layer["weights"]),
+            biases=np.array(layer["biases"])
+        ) for layer in layers]
+        return network
+
+    def save_to_file(self, filepath: str):
+        with open(filepath, "w") as f:
+            json.dump([{
+                "weights": layer.weights.tolist(),
+                "biases": layer.biases.tolist()
+            } for layer in self.layers], f)
+
+    @staticmethod
+    def __sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+
+    @staticmethod
+    def __sigmoid_derivative(x):
+        s = CustomNN.__sigmoid(x)
+        return s * (1 - s)
+
+    @staticmethod
+    def __cost(x, target):
+        return np.square(x - target).sum()
+
+    @staticmethod
+    def __cost_derivative(x, target):
+        return 2 * (x - target)
+
+    def eval(self, v):
+        for layer in self.layers:
+            v = self.__sigmoid(np.dot(layer.weights, v) + layer.biases)
+        return v
+
+    def __backpropagation(self, x, target):
+        gradient_w = []
+        gradient_b = []
+
+        # обход вперёд с сохранением промежуточных значений
+        activation = x
+        activations = [x]
+        zs = []
+        for layer in self.layers:
+            z = np.dot(layer.weights, activation) + layer.biases
+            zs.append(z)
+            activation = self.__sigmoid(z)
+            activations.append(activation)
+        cost = self.__cost(activation, target)
+
+        # вычисление градиента
+        delta = self.__cost_derivative(activations[-1], target) * self.__sigmoid_derivative(zs[-1])
+        gradient_b.append(delta)
+        gradient_w.append(np.dot(delta[:, np.newaxis], activations[-2][np.newaxis, :]))
+        for i in range(2, len(self.layers) + 1):
+            delta = np.dot(self.layers[-i + 1].weights.transpose(), delta) * self.__sigmoid_derivative(zs[-i])
+            gradient_b.append(delta)
+            gradient_w.append(np.dot(delta[:, np.newaxis], activations[-i - 1][np.newaxis, :]))
+
+        # Т.к. градиенты слоёв добавлялись в противоположном порядке, нужно перевернуть
+        gradient_b.reverse()
+        gradient_w.reverse()
+        return gradient_b, gradient_w, cost
+
+    def train(self,
+              training_data: pd.DataFrame,
+              iterations: int = 1000,
+              learning_rate: float = 0.1,
+              batch_size: int = 10,
+              seed: int = None,
+              testing_data: pd.DataFrame = None,
+              logger: clearml.Logger = None
+              ):
+        training_data = training_data.to_numpy()
+        if testing_data is not None:
+            testing_data = testing_data.to_numpy()
+        if seed is not None:
+            np.random.seed(seed)
+        for iteration in range(1, iterations + 1):
+            mean_cost = 0
+            # разбиваем данные на части
+            np.random.shuffle(training_data)
+            for j in range(0, len(training_data), batch_size):
+                batch = training_data[j:j + batch_size]
+                # вычисляем средний градиент для каждой части
+                gradient_b_sum = [np.zeros(layer.biases.shape) for layer in self.layers]
+                gradient_w_sum = [np.zeros(layer.weights.shape) for layer in self.layers]
+                for row in batch:
+                    x, target = row[1:], row[0:1]
+                    gradient_b, gradient_w, cost = self.__backpropagation(x, target)
+                    mean_cost += cost
+                    for i in range(len(self.layers)):
+                        gradient_b_sum[i] += gradient_b[i]
+                        gradient_w_sum[i] += gradient_w[i]
+                # применяем на веса и смещения
+                step_size = learning_rate / len(batch)
+                for i in range(len(self.layers)):
+                    self.layers[i].biases -= gradient_b_sum[i] * step_size
+                    self.layers[i].weights -= gradient_w_sum[i] * step_size
+            mean_cost /= len(training_data)
+            if testing_data is None:
+                print(f"[{time.strftime('%H:%M:%S')}] Итерация {iteration} выполнена, средняя ошибка: {mean_cost})")
+                if logger is not None:
+                    logger.report_scalar("Loss", "Train", iteration=iteration, value=mean_cost)
+            else:
+                # тестируем и вычисляем среднюю ошибку
+                test_mean_cost = 0
+                for row in testing_data:
+                    x, target = row[1:], row[0:1]
+                    test_mean_cost += self.__cost(self.eval(x), target)
+                test_mean_cost /= len(testing_data)
+                print(f"[{time.strftime('%H:%M:%S')}] Итерация {iteration} выполнена, средняя ошибка: {mean_cost}, средняя ошибка тестов: {test_mean_cost}")
+                if logger is not None:
+                    logger.report_scalar("Loss", "Train", iteration=iteration, value=mean_cost)
+                    logger.report_scalar("Loss", "Test", iteration=iteration, value=test_mean_cost)
---
+++ src/model/run_task.py
@@ -0,0 +1,113 @@
+import json
+import os
+import warnings
+import pandas as pd
+from clearml import Task, InputModel, OutputModel, Dataset
+from .neural_networks import CustomNN
+
+
+PROJECT_NAME = "ProjectX"
+
+
+# убирает предупреждения о больших числах в вычислениях от NumPy
+warnings.filterwarnings("ignore", "overflow encountered in exp", RuntimeWarning)
+
+
+def train_model(task, dataset, params):
+    dataset_metadata = dataset.get_metadata()
+    dataset_path = dataset.get_local_copy()
+    data = pd.read_csv(dataset_path + "/dataset.csv")
+    if 'Models/input_model_id' in params:
+        input_model = InputModel(params['Models/input_model_id'])
+        labels = input_model.labels
+        config = input_model.config_dict
+        config['input_model_id'] = params['Models/input_model_id']
+        network = CustomNN.from_file(input_model.get_weights())
+    else:
+        with open(dataset_path + "/labels.json", "r") as f:
+            labels = json.load(f)
+        config = {
+            'input_size':   int(dataset_metadata['input_size']),
+            'output_size':  int(dataset_metadata['output_size']),
+            'hidden_size':  int(params['Models/hidden_layer_size']),
+            'hidden_count': int(params['Models/hidden_layer_count']),
+            'random_seed':  int(params['Models/seed'])
+        }
+        network = CustomNN(**config)
+    network.train(
+        training_data=data[10:],
+        testing_data=data[:10],
+        iterations=int(params['Args/iterations']),
+        learning_rate=float(params['Args/learning_rate']),
+        batch_size=int(params['Args/batch_size']),
+        seed=int(params['Args/seed']),
+        logger=task.get_logger()
+    )
+    network.save_to_file("model.json")
+    output_model = OutputModel(
+        task=task,
+        name=params['Models/output_model_name'],
+        framework="CustomNN",
+        label_enumeration=labels,
+        config_dict=config
+    )
+    output_model.update_weights(
+        weights_filename="model.json",
+        async_enable=False,
+        upload_uri="https://files.clear.ml"
+    )
+    os.remove("model.json")
+
+
+def run_task():
+    task_id = input("Введите ID эксперимента: ")
+    task = Task.get_task(task_id=task_id)
+    task.mark_started()
+    params = task.get_parameters()
+    dataset = Dataset.get(dataset_id=params['Datasets/dataset_id'])
+    train_model(task, dataset, params)
+    task.mark_completed()
+
+
+def new_task():
+    task_name = input("Введите название эксперимента: ")
+    task = Task.init(project_name=PROJECT_NAME, task_name=task_name)
+    dataset_id = input("Введите ID датасета: ")
+    dataset = Dataset.get(dataset_id=dataset_id, alias="dataset_id")
+    input_model_id = input("Введите ID входной модели (0 - создать новую): ")
+    if input_model_id == "0":
+        print("Введите параметры модели")
+        model_params = {
+            'Models/hidden_layer_size':     int(input("\tРазмер скрытых слоёв: ")),
+            'Models/hidden_layer_count':    int(input("\tКол-во скрытых слоёв: ")),
+            'Models/seed':                  int(input("\tСемя: "))
+        }
+    else:
+        model_params = {
+            'Models/input_model_id': input_model_id
+        }
+        task.set_input_model(model_id=input_model_id)
+    output_model_name = input("Введите название выходной модели: ")
+    print("Введите параметры")
+    params = {
+        'Args/iterations':          int(input("\tИтерации: ")),
+        'Args/learning_rate':       float(input("\tСкорость обучения: ")),
+        'Args/batch_size':          int(input("\tРазмер частей: ")),
+        'Args/seed':                int(input("\tСемя: ")),
+        'Models/output_model_name': output_model_name
+    }
+    params.update(model_params)
+    task.set_parameters(params)
+    train_model(task, dataset, params)
+    task.close()
+
+
+def main():
+    inp = input("Введите 1, чтобы создать эксперимент, 2 - выполнить существующий эксперимент: ")
+    if inp == "1": new_task()
+    elif inp == "2": run_task()
+    else: raise ValueError("Invalid input")
+
+
+if __name__ == "__main__":
+    main()
